e0ac6559b7e8fdae70cbe76983eb8f5ccd09fc91
RGCENA <raviganesh387@gmail.com>
2024-09-12 11:42:09 +0530

    js

--- goku.js
+++ goku.js
@@ -76,3 +76,18 @@ const rgshop = async () => {
 }
 
 rgshop();
+
+const fshop = async() => {
+    fetch('https://dummyjson.com/products')
+    .then(async (e) => {
+        let response = await e.json();
+        let products = response.products;
+        console.log(products);
+    }
+    )
+    .catch((error)=>{
+        console.log(error);
+    })
+}
+
+fshop();
